924664fff8100ce0aa234b63196c47490cf6787e
brookie guzder-williams <bguzder-williams@berkeley.edu>
2026-05-14 22:10:38 -0700

    v0.6.3: builder bug fixes

--- jupyter_bioacoustic/labextension/static/remoteEntry.f0824c481359f93e0445.js
+++ jupyter_bioacoustic/labextension/static/remoteEntry.f0824c481359f93e0445.js
@@ -222,7 +222,7 @@ __webpack_require__.d(exports, {
 /******/ 			var promises = [];
 /******/ 			switch(name) {
 /******/ 				case "default": {
-/******/ 					register("jupyter-bioacoustic", "0.6.2", () => (__webpack_require__.e("lib_index_js").then(() => (() => (__webpack_require__(/*! ./lib/index.js */ "./lib/index.js"))))));
+/******/ 					register("jupyter-bioacoustic", "0.6.3", () => (__webpack_require__.e("lib_index_js").then(() => (() => (__webpack_require__(/*! ./lib/index.js */ "./lib/index.js"))))));
 /******/ 				}
 /******/ 				break;
 /******/ 			}
@@ -534,4 +534,4 @@ __webpack_require__.d(exports, {
 /******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=remoteEntry.aaea853ec5bb433d0bc9.js.map
+//# sourceMappingURL=remoteEntry.f0824c481359f93e0445.js.map
