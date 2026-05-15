74f594e666573a158774c4fdd6ec47cd1c36e77c
brookie guzder-williams <bguzder-williams@berkeley.edu>
2026-05-15 21:09:34 +0000

    __init__.py: move debug logging setup to parent logger for inheritance
    Debug logging was only attached to config_builder child logger.
    Moving to parent jupyter_bioacoustic logger ensures all child
    loggers (including audio) inherit the debug handler.

--- jupyter_bioacoustic/__init__.py
+++ jupyter_bioacoustic/__init__.py
@@ -1,3 +1,20 @@
+import logging
+import os
+import sys
+
+_log = logging.getLogger('jupyter_bioacoustic')
+
+if os.environ.get('JBA_DEBUG_MODE'):
+    _log_path = os.path.join(os.environ.get('JBA_LOG_DIR', '.'), 'jba_debug.log')
+    _handler = logging.FileHandler(_log_path)
+    _handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s %(name)s: %(message)s'
+    ))
+    _log.addHandler(_handler)
+    _log.setLevel(logging.DEBUG)
+    sys.__stderr__.write(f'[JBA] Debug mode enabled. Logs → {os.path.abspath(_log_path)}\n')
+    sys.__stderr__.flush()
+
 from .api import BioacousticAnnotator
 from .config_builder import ConfigBuilder
 
