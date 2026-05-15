f95f781ce85787a4b941c2815067a2d3ceb56794
brookie guzder-williams <bguzder-williams@berkeley.edu>
2026-05-15 21:10:06 +0000

    core.py: remove local debug setup (now inherited from parent logger)

--- jupyter_bioacoustic/config_builder/core.py
+++ jupyter_bioacoustic/config_builder/core.py
@@ -10,24 +10,13 @@ import json
 import logging
 import os
 import re
-import sys
+
 import uuid
 
 from IPython import get_ipython
 from IPython.display import display, HTML
 
 _log = logging.getLogger('jupyter_bioacoustic.config_builder')
-
-if os.environ.get('JBA_DEBUG_MODE'):
-    _log_path = os.path.join(os.environ.get('JBA_LOG_DIR', '.'), 'jba_debug.log')
-    _handler = logging.FileHandler(_log_path)
-    _handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s %(name)s: %(message)s'
-    ))
-    _log.addHandler(_handler)
-    _log.setLevel(logging.DEBUG)
-    sys.__stderr__.write(f'[JBA] Debug mode enabled. Logs → {os.path.abspath(_log_path)}\n')
-    sys.__stderr__.flush()
 
 #
 # CONSTANTS
