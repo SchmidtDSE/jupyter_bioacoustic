d7ff7bd56afc6ce5ec144fe8ae353d102c961b16
brookie guzder-williams <bguzder-williams@berkeley.edu>
2026-05-17 14:12:19 +0000

    audio/__init__.py: restyle to project code standards

--- jupyter_bioacoustic/audio/__init__.py
+++ jupyter_bioacoustic/audio/__init__.py
@@ -1,1 +1,10 @@
+"""
+Audio (Package)
+
+Unified IO for reading and writing audio across local,
+S3, GCS, and HTTPS backends.
+
+License: BSD 3-Clause
+"""
+
 from .io import read, read_segment, write, list_files
