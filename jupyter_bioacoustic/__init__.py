57c87587d8969d57486f3b916160f8090e4c057f
brookie guzder-williams <bguzder-williams@berkeley.edu>
2026-05-17 14:12:17 +0000

    __init__.py: restyle to project code standards

--- jupyter_bioacoustic/__init__.py
+++ jupyter_bioacoustic/__init__.py
@@ -1,3 +1,12 @@
+"""
+Jupyter Bioacoustic
+
+JupyterLab extension for bioacoustic audio annotation
+and review.
+
+License: BSD 3-Clause
+"""
+
 import logging
 import os
 import sys
@@ -5,14 +14,20 @@ import sys
 _log = logging.getLogger('jupyter_bioacoustic')
 
 if os.environ.get('JBA_DEBUG_MODE'):
-    _log_path = os.path.join(os.environ.get('JBA_LOG_DIR', '.'), 'jba_debug.log')
+    _log_path = os.path.join(
+        os.environ.get('JBA_LOG_DIR', '.'),
+        'jba_debug.log',
+    )
     _handler = logging.FileHandler(_log_path)
     _handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s %(name)s: %(message)s'
+        '%(asctime)s %(levelname)s %(name)s: %(message)s',
     ))
     _log.addHandler(_handler)
     _log.setLevel(logging.DEBUG)
-    sys.__stderr__.write(f'[JBA] Debug mode enabled. Logs → {os.path.abspath(_log_path)}\n')
+    sys.__stderr__.write(
+        f'[JBA] Debug mode enabled. '
+        f'Logs \u2192 {os.path.abspath(_log_path)}\n',
+    )
     sys.__stderr__.flush()
 
 from .api import BioacousticAnnotator
