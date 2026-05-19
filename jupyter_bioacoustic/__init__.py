2d2ce1c2920e04b9225c2d7bb5a1d6be8ebf8fef
brookie guzder-williams <bguzder-williams@berkeley.edu>
2026-05-19 21:27:44 +0000

    __init__.py: resilient file handler for debug logging

--- jupyter_bioacoustic/__init__.py
+++ jupyter_bioacoustic/__init__.py
@@ -15,12 +15,26 @@ _log = logging.getLogger('jupyter_bioacoustic')
 
 DEFAULT_LOG_FILENAME = 'jba_debug.log'
 
+
+class _ResilientFileHandler(logging.FileHandler):
+    """FileHandler that recovers when the log file is deleted or replaced."""
+
+    def emit(self, record: logging.LogRecord) -> None:
+        try:
+            if self.stream and (self.stream.closed or not os.path.exists(self.baseFilename)):
+                self.stream.close()
+                self.stream = self._open()
+        except Exception:
+            pass
+        super().emit(record)
+
+
 if os.environ.get('JBA_DEBUG_MODE'):
     _log_path = os.path.join(
         os.environ.get('JBA_LOG_DIR', '.'),
         os.environ.get('JBA_LOG_FILE', DEFAULT_LOG_FILENAME),
     )
-    _handler = logging.FileHandler(_log_path)
+    _handler = _ResilientFileHandler(_log_path)
     _handler.setFormatter(logging.Formatter(
         '%(asctime)s %(levelname)s %(name)s: %(message)s',
     ))
